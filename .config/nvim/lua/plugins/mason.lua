fce940df13e72e971ee3a00f9481be0e52498ac1
Belal Elsabbagh <belsabbagh@gmail.com>
2025-03-20 14:12:53 +0200

    removed unused plugins

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -4,6 +4,15 @@ return {
     ensure_installed = {
       'typescript-language-server',
       'biome',
+      'svelte-language-server',
+      'vue-language-server',
+      'gopls',
+      'rust-analyzer',
+      'stylua',
+      'html-lsp',
+      'ruff',
+      'mypy',
+      'pyright',
     },
   },
 }
