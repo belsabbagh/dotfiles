4a30f4e705893ddf1cdf33ad53e4a2135869c95f
Belal Elsabbagh <belsabbagh@gmail.com>
2025-05-14 15:21:33 +0300

    fixed ruff formatter

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -44,6 +44,33 @@ return {
         stdin = true,
         args = { 'format', '--stdin-file-path', '$FILENAME' },
       },
+      ---@type conform.FileFormatterConfig
+      ruff = {
+        meta = {
+          url = 'https://docs.astral.sh/ruff/',
+          description = 'An extremely fast Python linter, written in Rust. Formatter subcommand.',
+        },
+        command = 'ruff',
+        args = {
+          'format',
+          '--force-exclude',
+          '--stdin-filename',
+          '$FILENAME',
+          '-',
+        },
+        range_args = function(self, ctx)
+          return {
+            'format',
+            '--force-exclude',
+            '--range',
+            string.format('%d:%d-%d:%d', ctx.range.start[1], ctx.range.start[2] + 1, ctx.range['end'][1], ctx.range['end'][2] + 1),
+            '--stdin-filename',
+            '$FILENAME',
+            '-',
+          }
+        end,
+        stdin = true,
+      },
       intelephense = {
         command = 'intelephense',
         args = {},
