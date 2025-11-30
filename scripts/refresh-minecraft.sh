5fc1a4af29247e9af222cb461fcea4935ec8a3df
Belal Elsabbagh <belsabbagh@gmail.com>
2025-11-30 19:14:48 +0200

    fixed refresh minecraft

--- scripts/refresh-minecraft.sh
+++ scripts/refresh-minecraft.sh
@@ -1,12 +1,1 @@
-#back up
-cp -r .minecraft .minecraft-backup
-
-#delete
-rm -rf .minecraft
-
-mkdir -p .minecraft
-
-#restore saves
-cp -r .minecraft-backup/saves .minecraft/saves
-
-cp -r .minecraft-backup/options.txt .minecraft/options.txt
+rm -rf .minecraft/webcache2
