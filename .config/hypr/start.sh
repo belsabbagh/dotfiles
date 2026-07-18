c8d602b947d58f07c1ccb57f63410c07c1c0c655
Belal Elsabbagh <belal@belsabbagh.me>
2026-07-18 19:34:00 +0300

    stuff

--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -23,7 +23,7 @@ echo "--- Hyprland Startup: $(date) ---"
 run_and_time qs -c caelestia
 # run_and_time systemctl --user start hyprpaper
 # run_and_time systemctl --user start hypridle
-# run_and_time systemctl --user start hyprpolkitagent
+run_and_time systemctl --user start hyprpolkitagent
 run_and_time systemctl --user start xdg-desktop-portal-hyprland
 run_and_time ssh-agent
 run_and_time copyq
---
+++ scripts/dancing-line.sh
@@ -0,0 +1,1 @@
+WINEPREFIX=/home/belal/Games/itchio/dancing-line-community-edition ~/Games/itchio/dancing-line-community-edition/drive_c/dancing-line-community-edition/Dancing\ Line.exe
